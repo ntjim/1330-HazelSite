5dc0db7049811c7b05201607e40794060b5d5ff4
chull1589 <chull1589@gmail.com>
2021-11-26 17:19:34 -0500

    implement log in screen

--- hazel/ios/Flutter/flutter_export_environment.sh
+++ hazel/ios/Flutter/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=C:\Users\eizag\Documents\flutter"
-export "FLUTTER_APPLICATION_PATH=C:\Users\eizag\Documents\arbor\1330-ArborSite\hazel"
+export "FLUTTER_ROOT=C:\Users\user\Documents\flutter"
+export "FLUTTER_APPLICATION_PATH=C:\Hazel\1330-HazelSite\hazel"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_TARGET=lib\main.dart"
 export "FLUTTER_BUILD_DIR=build"

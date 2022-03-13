cbb5c9ae3f053a61f48a547aa2999fde5e7d0751
Megan Chen <78578157+meganc46@users.noreply.github.com>
2022-03-12 23:04:39 -0500

    Remove unused imports

--- hazel/lib/change_password_form.dart
+++ hazel/lib/change_password_form.dart
@@ -1,10 +1,6 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart' show rootBundle;
-import 'package:hazel/user_settings.dart';
-import 'package:provider/provider.dart';
 
 import './user_account_settings.dart';
 
--- hazel/lib/project_search.dart
+++ hazel/lib/project_search.dart
@@ -3,18 +3,9 @@ import 'dart:async';
 
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart' show rootBundle;
 import 'package:hazel/nav_bar.dart';
-import 'package:provider/provider.dart';
 
-import 'firebase_options.dart';
-
-import './home.dart';
-import './user_settings.dart';
-import './me_page.dart';
-import './login_valid.dart';
 import './nav_bar.dart';
 import './project_page.dart';
 

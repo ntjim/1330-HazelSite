8f930c238e4b3f472c9a88575965c1bf4c93278d
Youngchull Koh <chull1589@gmail.com>
2022-03-12 20:37:46 -0500

    change home widget back to HomePage

--- hazel/lib/main.dart
+++ hazel/lib/main.dart
@@ -48,7 +48,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: navColor,
       ),
-      home: const HomePage(),
+      home: const CartPage(),
     );
   }
 }
